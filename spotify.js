45c3fca62d676048e98e56aac1eb4639dd59042f
Mujaffar <mdmujjuhyd7866@gmail.com>
2023-03-14 21:25:45 +0530

    Second commit

---
+++ spotify.js
@@ -0,0 +1,10 @@
+let a=document.getElementsByTagName("div");
+Array.from(a).forEach(a=>{
+console.log(a)
+a.style.color="red"
+})
+//a.style.color="red";
+//a.style.backgroundColor="blue"
+//a.style.borderRadius="40px"
+//a.innerText="muzzu is a good boy"
+//a.innerHTML="<b>muzzu is a good boy</b>"
